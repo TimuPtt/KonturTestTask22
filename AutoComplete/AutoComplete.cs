c5e5fed79165cf691b08f1459a31a30ce8d571dd
Timofei Shchukin <timuptt@gmail.com>
2022-03-05 19:42:00 +0300

    Add files

---
+++ AutoComplete/AutoComplete.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoComplete
+{
+    public struct FullName
+    {
+        public string Name;
+        public string Surname;
+        public string Patronymic;
+
+        public override string ToString()
+        {
+            return $"{Surname?.Trim()} {Name?.Trim()} {Patronymic?.Trim()}".Trim();
+        }
+    }
+
+    public class AutoCompleter
+    {   
+        /// <summary>
+        /// Trie (prefix tree) container for full names
+        /// </summary>
+        private class Trie
+        {
+            /// <summary>
+            /// Representation of one node (symbol) in Trie
+            /// </summary>
+            private class Node
+            {
+                public char Symbol { get; set; }
+                public Dictionary<char, Node>? Children { get; set; }
+                public bool IsFullName { get; set; }
+            }
+
+            private readonly Node _root;
+
+            public Trie()
+            {   
+                _root = new Node();
+            }
+
+            /// <summary>
+            /// Adds name to Trie
+            /// </summary>
+            /// <param name="name">Name</param>
+            public void AddName(string name)
+            {   
+                var current = _root;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (current.Children != null && current.Children.TryGetValue(name[i], out var node))
+                    {
+                        current = node;
+                    }
+                    else
+                    {
+                        if (current.Children == null)
+                        {
+                            current.Children = new Dictionary<char, Node>();
+                        }
+                        current.Children.Add(name[i], current = new Node() { Symbol = name[i] });
+                    }
+                }
+                current.IsFullName = true;
+            }
+
+            /// <summary>
+            /// Find all names that starts with prefix
+            /// </summary>
+            /// <param name="prefix">Prefix</param>
+            /// <returns>Collections of names</returns>
+            public IEnumerable<string> StartsWith(string prefix)
+            {
+                var node = GetNode(prefix);
+                if (node == null)
+                {
+                    return Enumerable.Empty<string>();
+                }
+                return GetNames(new StringBuilder().Append(prefix.AsSpan(0, prefix.Length - 1)), node);
+            }
+
+            /// <summary>
+            /// Get name from Trie by building it from nodes
+            /// </summary>
+            /// <param name="builder">String builder</param>
+            /// <param name="node">Node</param>
+            /// <returns>Name</returns>
+            private IEnumerable<string> GetNames(StringBuilder builder, Node node)
+            {
+                builder.Append(node.Symbol);
+                if (node.IsFullName)
+                {   
+                    yield return builder.ToString();
+                }
+
+                if (node.Children == null)
+                {
+                    builder.Remove(builder.Length - 1, 1);
+
+                    yield break;
+                }
+
+                foreach (var childNode in node.Children.Values)
+                {
+                    foreach (var name in GetNames(builder, childNode))
+                    {
+                        yield return name;
+                    }
+                }
+
+                builder.Remove(builder.Length - 1, 1);
+            }
+            
+            /// <summary>
+            /// Get endname node from Trie
+            /// </summary>
+            /// <param name="prefix"></param>
+            /// <returns></returns>
+            private Node GetNode(string prefix)
+            {
+                var current = _root;
+                for (int i = 0; i < prefix.Length; i++)
+                {
+                    if (current.Children != null && current.Children.TryGetValue(prefix[i], out var node))
+                    {
+                        current = node;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                return current;
+            }
+        }
+
+        private readonly Trie _fullNamesContainer = new Trie();
+
+        public void AddToSearch(List<FullName> fullNames)
+        {
+            foreach (var fullName in fullNames)
+            {   
+                _fullNamesContainer.AddName(fullName.ToString());
+            }
+        }
+
+        public List<string> Search(string prefix)
+        {   
+            if (string.IsNullOrWhiteSpace(prefix))
+            {
+                throw new ArgumentNullException(nameof(prefix), "Parameter cant be null or contain only white-spaces");
+            }
+
+            if (prefix.Length > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(prefix), "Parameter length cant be more than 100");
+            }
+
+            prefix = string.Join(' ', prefix.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            
+            return _fullNamesContainer.StartsWith(prefix).ToList();
+        }
+    }
+}
---
+++ AutoComplete/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoComplete
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fullNames = new List<FullName>
+            {
+              new FullName {Name = "Иван", Surname = "Двитриев1", Patronymic = "Сергеевич"},
+              new FullName {Name = "Иван", Surname = null, Patronymic = "Сергеевич"},
+              //new FullName {Name = "Семен", Surname = "Семенов", Patronymic = "Сергеевич"},
+              new FullName {Name = "Иван", Surname = "Двитриев", Patronymic = "Сергеевич"},
+              new FullName {Name = "Иван", Surname = "Семенов", Patronymic = "Сергеевич"},
+              new FullName {Name = null, Surname = "Семенов", Patronymic = null}
+            };
+
+            var autoCompleter = new AutoCompleter();
+            autoCompleter.AddToSearch(fullNames);
+
+            fullNames = new List<FullName>
+            {
+              new FullName {Name = "Иван", Surname = "Двитриев1", Patronymic = "Сергеевич"},
+              new FullName {Name = "Георгий", Surname = "Семенов", Patronymic = "Сергеевич"},
+              new FullName {Name = "Георгий", Surname = "Семенов", Patronymic = null},
+              new FullName {Name = "Геннадий", Surname = "Семенов", Patronymic = null},
+              new FullName {Name = null, Surname = "Сенов", Patronymic = null},
+              new FullName {Name = null, Surname = "Сумка", Patronymic = null}
+
+            };
+            autoCompleter.AddToSearch(fullNames);
+            //var fullNames = new List<FullName>()
+            //{
+            //    new FullName()
+            //    {
+            //        Name = null,
+            //        Surname = "Сюткин",
+            //        Patronymic = null
+            //    },
+            //    new FullName()
+            //    {
+            //        Name = null,
+            //        Surname = "Сютов",
+            //        Patronymic = null
+            //    },
+            //    new FullName()
+            //    {
+            //        Name = null,
+            //        Surname = "Салкин",
+            //        Patronymic = null
+            //    },
+            //    new FullName()
+            //    {
+            //        Name = null,
+            //        Surname = "Светлов",
+            //        Patronymic = null
+            //    }
+            //};
+            //autoCompleter.AddToSearch(fullNames);
+
+            //foreach (var val in autoCompleter.Search("Салк"))
+            //{
+            //    Console.WriteLine(val);
+            //}
+            //Console.WriteLine("-------------------------------------------");
+
+            foreach (var val in autoCompleter.Search("С"))
+            {
+                Console.WriteLine(val);
+            }
+
+        }
+    }
+}
